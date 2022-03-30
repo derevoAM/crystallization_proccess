4f4d4959a48744808386f16b3d340611390620c7
Esaak <gerasimov.ia@phystech.edu>
2022-03-30 08:56:16 +0300

    add .gitignore

--- Points.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by п on 24.03.2022.
-//
-
-#include "Points.h"
-Points:: Points (): size(3),heat_capacity(0), impurity(0), coordinates(0, 0)  {}
-Points::Points(double size, double heat_capacity, double impurity, double x, double y): size(size),heat_capacity(heat_capacity), impurity(impurity), coordinates(x, y) {}
